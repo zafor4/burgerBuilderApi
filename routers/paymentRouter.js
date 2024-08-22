b91a2705ff549b8017a50c88cf4beddfe225d0fc
Zafor Iqbal <xoy4444@gmail.com>
2024-08-22 21:46:01 +0600

    may be

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -4,7 +4,7 @@ const PaymentSession = SSLCommerz.PaymentSession;
 const authorized=require('../middlewares/authorize')
 const {Order}=require('../models/order');
 const generateUniqueId = require('generate-unique-id');
-const Payment=require('../models/payment')
+const {Payment}=require('../models/payment')
 
 
 
